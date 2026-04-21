516e97a1f5687d08a3b8dcb062a2a3a38d64f920
musnymubarak <musnymohammed@gmail.com>
2026-04-21 21:36:54 +0530

    Dokcer setup for deployment

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+    base: '/admin/',
     plugins: [react()],
     resolve: {
         alias: {
