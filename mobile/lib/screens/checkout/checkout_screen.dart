8c0d6d5568f95f5eb10a056685f8e29e6622c377
musnymubarak <musnymohammed@gmail.com>
2026-06-29 09:03:26 +0000

    fix(ios): remove applePay to prevent merchantIdentifier error

--- mobile/lib/screens/checkout/checkout_screen.dart
+++ mobile/lib/screens/checkout/checkout_screen.dart
@@ -268,7 +268,6 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
           paymentSheetParameters: SetupPaymentSheetParameters(
             paymentIntentClientSecret: clientSecret,
             merchantDisplayName: 'Daily Grocer',
-            applePay: const PaymentSheetApplePay(merchantCountryCode: 'GB'),
             style: ThemeMode.light,
           ),
         );
