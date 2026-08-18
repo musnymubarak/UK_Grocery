c30f22214ba1a0eaddcb80a69f15e4b29de89187
musnymubarak <musnymohammed@gmail.com>
2026-08-18 12:21:37 +0000

    fix(mobile): resolve api_registry import path and CupertinoPageTransitionsBuilder import

--- mobile/lib/core/services/push_notification_service.dart
+++ mobile/lib/core/services/push_notification_service.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import '../network/api_registry.dart';
+import '../../data/api/api_registry.dart';
 import '../router/app_router.dart';
 
 @pragma('vm:entry-point')
